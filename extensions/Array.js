981315b7c3b19f5b021f2978bdce4f9574bb5f2d
Devang <devang.sg@kratee.com>
2018-08-16 01:15:15 +0530

    added/updated extensions

--- extensions/Array.js
+++ extensions/Array.js
@@ -1,4 +1,4 @@
-Array.prototype.remove =function (item) {
+Array.prototype.remove = function (item) {
 	// Find and remove item from an array
 	var i = this.indexOf(item);
 	if(i != -1) {
---
+++ extensions/Date.js
@@ -0,0 +1,25 @@
+Date.prototype.getDelimeter = function() {
+  let number = this.getDate();
+  var lastChar = number.toString().split('').pop(); // gives string
+
+  if( lastChar == 1 && number != 11 ) {
+    return 'st';
+  } else if( lastChar == 2 ) {
+    return 'nd';
+  } else if (lastChar == 3) {
+    return 'rd';
+  }
+
+  return 'th';
+}
+
+console.log('vfvf');
+
+Date.prototype.getMonthText = function() {
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  return monthNames[this.getMonth()];
+}
