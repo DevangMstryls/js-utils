6c86882432f4b533d81009f3c8b1bcb12ca82364
Devang <devang@oorjan.com>
2019-01-25 20:21:43 +0530

    fixed the sub menu functionality, added url change on tab change

--- utils/accordion.js
+++ utils/accordion.js
@@ -1,13 +1,15 @@
-export default function pmtAccordion(userOptions) {
-	var defaultOptions = {
+export function pmtAccordion(userOptions) {
+	let defaultOptions = {
 		openOnlyOne: true,
 		contCls: '.acc-cont',
 		headCls: '.acc-head',
 		bodyCls: '.acc-body',
+		onOpen: function () {},
+		onClose: function () {},
 	};
-	var options = jQuery.extend({}, defaultOptions, userOptions);
-	
-	var openClsName = 'opened';
+	let options = jQuery.extend({}, defaultOptions, userOptions);
+
+	let openClsName = 'opened';
 	// var openCls = '.' + openClsName;
 	
 	// pass a selector that is nested under a container
@@ -15,15 +17,23 @@ export default function pmtAccordion(userOptions) {
 		
 		click: function () {
 			
-			var $this = jQuery(this);
-			var group = $this.data('group');
-			var $accItemBody = jQuery(options.bodyCls + '[data-group="' + group + '"]');
-			var $accCont = $this.closest(options.contCls);
+			let $this = jQuery(this);
+			let group = $this.data('group');
+			let $accItemBody = jQuery(options.bodyCls + '[data-group="' + group + '"]');
+			let $accCont = $this.closest(options.contCls);
 			
 			if ($this.hasClass(openClsName)) {
 				// hide current
 				$accItemBody.slideUp();
 				$this.removeClass(openClsName);
+				if( typeof options.onClose === 'function') {
+					options.onClose({
+						data:{group}
+					});
+				}
+				else {
+					console.info('onClose is not a function');
+				}
 			}
 			else {
 				if (options.openOnlyOne) {
@@ -36,10 +46,152 @@ export default function pmtAccordion(userOptions) {
 				$accItemBody.slideDown();
 				if ($this.hasClass('has-submenus')) {
 					$this.addClass(openClsName);
+					if( typeof options.onOpen === 'function') {
+						options.onOpen({
+							data:{group}
+						});
+					}
+					else {
+						console.info('onOpen is not a function');
+					}
 				}
 			}
 			
 		}
 		
 	});
+}
+
+export class PmtAccordion {
+	defaultOptions = {
+		openOnlyOne: true,
+		contCls: '.acc-cont',
+		headCls: '.acc-head',
+		bodyCls: '.acc-body',
+		onOpen: undefined,
+		onClose: undefined,
+	};
+	openClsName = 'opened';
+	options = {};
+	userOptions = {};
+
+	contextVars = {};
+
+	constructor(userOptions) {
+		this.userOptions = userOptions;
+		this.options = jQuery.extend({}, this.defaultOptions, this.userOptions);
+
+		this.init();
+	}
+
+	init() {
+		this._handleClick();
+	}
+
+	_setContextVars(group) {
+		this.contextVars['group'] = group;
+		this.contextVars['$accItemHead'] = jQuery(this.options.headCls + '[data-group="' + group + '"]');
+		this.contextVars['$accItemBody'] = jQuery(this.options.bodyCls + '[data-group="' + group + '"]');
+		this.contextVars['$accCont'] = this.contextVars.$accItemHead.closest(this.options.contCls);
+	}
+
+	/**
+	 * For expanding a particular panel externally
+	 * */
+	expand(group) {
+		this._setContextVars(group);
+		this._expandCurrent();
+	}
+
+	/**
+	 * For collapsing a particular panel externally
+	 * */
+	collapse(group) {
+		this._setContextVars(group);
+		this._collapseCurrent();
+	}
+
+	/**
+	 * This is completely dependent on the current context vars
+	 * */
+	_expandCurrent() {
+		// show current
+
+		this.contextVars.$accItemBody.slideDown();
+		this.contextVars.$accItemHead.addClass(this.openClsName);
+
+		let self = this;
+
+		// callback
+		if( typeof this.options.onOpen === 'function') {
+			this.options.onOpen({
+				data:{
+					group: self.contextVars.group,
+				}
+			});
+		}
+		else {
+			// console.info('onOpen is not a function');
+		}
+	}
+
+	/**
+	 * This is completely dependent on the current context vars
+	 * */
+	_collapseCurrent() {
+		// hide current
+		this.contextVars.$accItemBody.slideUp();
+		this.contextVars.$accItemHead.removeClass(this.openClsName);
+
+		let self = this;
+
+		// callback
+		if( typeof this.options.onClose === 'function') {
+			this.options.onClose({
+				data:{
+					group: self.contextVars.group,
+				}
+			});
+		}
+		else {
+			// console.info('onClose is not a function');
+		}
+	}
+
+	/**
+	 * This is completely dependent on the current context vars
+	 * */
+	_collapseAll() {
+		// find all the accordion body els in this container and close them
+		this.contextVars.$accCont.find(this.options.bodyCls).slideUp();
+		// find all the accordion head els in this container and remove the open class from them
+		this.contextVars.$accCont.find(this.options.headCls).removeClass(this.openClsName);
+	}
+
+	_handleClick() {
+		let self = this;
+
+		// pass a selector that is nested under a container
+		jQuery(self.options.headCls).on({
+			click: function () {
+
+				let $this = jQuery(this);
+				let group = $this.data('group');
+				self._setContextVars(group);
+
+				if ($this.hasClass(self.openClsName)) {
+					self._collapseCurrent();
+				}
+				else {
+					if (self.options.openOnlyOne) {
+						// hide all
+						self._collapseAll();
+					}
+
+					self._expandCurrent();
+				}
+			}
+		});
+	}
+
 }
